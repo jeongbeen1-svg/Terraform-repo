d803eff23a67c2b01d4d16034fdc2592331a2729
jeongbeen1-svg <jeongbeen1@gmail.com>
2026-05-19 17:39:07 +0900

    ch07:gallery-05

---
+++ ch07/05 gallery-05/local.tf
@@ -0,0 +1,29 @@
+locals {
+  org         = "tf-core"
+  project     = "gallery"
+  environment = var.env
+
+  namespace = "${local.org}-${local.project}-${local.environment}"
+
+  infra = {
+    lb = {
+      listener_port = 80
+    }
+
+    lt = {
+      service_port   = 8080
+      instance_type = var.infra_lt_instance_type
+
+    }
+    
+
+    asg = {
+      max_size = var.infra_asg_max_size
+      min_size = var.infra_asg_min_size
+      desired_capacity = var.infra_asg_desired_capacity
+      deploy_version = "1.0.0"
+
+    }
+
+    }
+  }
