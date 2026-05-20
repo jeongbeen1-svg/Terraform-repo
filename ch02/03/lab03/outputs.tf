adf41b9bb9f1e7d96d8fb5c8323ea2a29734ffcd
jeongbeen1-svg <jeongbeen1@gmail.com>
2026-05-20 22:48:47 +0900

    add: annotation training

--- ch02/03/lab03/outputs.tf
+++ ch02/03/lab03/outputs.tf
@@ -1,14 +1,20 @@
+# 생서된 리소스 정보를 외부로 출력
+# → 루트모듈에서는 콘솔 출력, 상위모듈에서는 module.xxx.yyy 로 참조 가능
+
+# 인스턴스 ID 출력
 output "instance" {
     value = aws_instance.instance.id
 }
+# 시큐리티그룹 ID 출력
 output "sg" {
     value = aws_security_group.instance.id  
 }
-
+# iam 프로필 이름 출력
+# .id가 아닌 .name 인 이유: 프로필은 이름으로 EC2에 연결하기 때문
 output "iamprofile" {
     value = aws_iam_instance_profile.instance.name  
 }
-
+# iam 역할 ID 출력
 output "iamrole" {
     value = aws_iam_role.instance.id
 }
--- ch02/04/lab01/local.tf
+++ ch02/04/lab01/local.tf
@@ -1,6 +1,8 @@
 locals {
   project = "04-lab01"
 
+
+  # IAM Role 관련 값을 객체로 묶어서 관리
   iamrole = {
     name = "instance"
 
@@ -15,16 +17,19 @@ locals {
     }]
   })
 
+  # 정의된 정책은 main.tf와 연결 (ssm 접근하기 위한 정책)
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   }
   
   instance = {
     name = "ssm-test"
     
+    # main.tf에서 정의한 local ec2 스펙
     ami = "ami-0c003e98ceffee43e"
     instance_type = "t3.micro"
     associate_public_ip_address = true
 
+    # 포트도 정의
     allow_access = {
       port = 80
       cidr_blocks = ["0.0.0.0/0"]
