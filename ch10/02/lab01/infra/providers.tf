5b7621e697521d3399aa5e09f947ff4e0a4e63c1
jeongbeen1-svg <jeongbeen1@gmail.com>
2026-05-21 11:39:01 +0900

    s3 fix

--- ch10/02/lab01/infra/providers.tf
+++ ch10/02/lab01/infra/providers.tf
@@ -9,7 +9,7 @@ terraform {
   }
   
   backend "s3" {
-    bucket       = "tf-core-tfstate"
+    bucket       = "tf-core-tfstate-5616519818199"
     key          = "10.02/lab01/terraform.tfstate"
     region       = "ap-northeast-2"
     encrypt      = true
